9fd56fe732f0035f731870cbafb628373e0ea929
Tamariiiis <63346306+Tamariiiis@users.noreply.github.com>
2020-05-11 09:27:48 +0200

    v.03

---
+++ projekatOOP/Model/Kvadratic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace projekatOOP.Model
+{
+    class Kvadratic
+    {
+		private Rectangle rect;
+
+		public Rectangle Rect
+		{
+			get { return rect; }
+			set { rect = value; }
+		}
+
+		private double size;
+
+		public double Size
+		{
+			get { return size; }
+			set { size = value; }
+		}
+
+		private bool pin;
+
+		public bool Pin
+		{
+			get { return pin; }
+			set { pin = value; }
+		}
+
+
+		public Kvadratic(byte r, byte g, byte b)
+		{
+			pin = false;
+			size = 10;
+			rect = new Rectangle();
+			rect.Width = size;
+			rect.Height = size;
+			Color c = Color.FromRgb(r, g, b);
+			SolidColorBrush bb = new SolidColorBrush(c);
+			Brush boja = bb;
+			rect.Fill = boja;
+		}
+
+	}
+}
---
+++ projekatOOP/Model/Tabela.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekatOOP.Model
+{
+    class Tabela
+    {
+		private Kvadratic[,] matrica;
+
+		public Kvadratic[,] Matrica
+		{
+			get { return matrica; }
+			set { matrica = value; }
+		}
+
+		private void Pin()
+		{
+			matrica[0, 0].Pin = true;
+
+			matrica[matrica.GetLength(0) - 1, 0].Pin = true;
+
+			matrica[0, matrica.GetLength(0) - 1].Pin = true;
+
+			matrica[matrica.GetLength(0) - 1, matrica.GetLength(0) - 1].Pin = true;
+		}
+
+		public Tabela(int m, int n)
+		{
+			matrica = new Kvadratic[m, n];
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					matrica[i, j] = new Kvadratic();
+				}
+
+			}
+		}
+	}
+}
